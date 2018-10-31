75fdfd59900395bc42b0e8394312d1e7a84329e5
BHunter2889 <bhunter8989@icloud.com>
2018-10-31 01:11:45 -0500

    fishing forecast srvc impl

--- main.go
+++ main.go
@@ -4,20 +4,20 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/BHunter2889/da-fish-alexa/alexa"
 	"github.com/BHunter2889/da-fish-alexa/services"
-	"net/url"
 	"net/http"
-	"io/ioutil"
-	"encoding/xml"
 	"log"
+	"fmt"
 )
+
 // TODO - add context.Context for xray tracing
 
 var (
-	cfg *DaFishConfig
+	cfg           *DaFishConfig
 	defaultUserIp = "127.0.0.1"
 
-	DeviceLocService services.DeviceService
-	GeocodeService	services.GeocodeService
+	DeviceLocService  services.DeviceService
+	GeocodeService    services.GeocodeService
+	ForecasterService services.ForecasterService
 )
 
 func IntentDispatcher(request alexa.Request) alexa.Response {
@@ -25,10 +25,10 @@ func IntentDispatcher(request alexa.Request) alexa.Response {
 	switch request.Body.Intent.Name {
 	case "TodaysFishRatingIntent":
 		response = HandleTodaysFishRatingIntent(request)
-	case "FrontpageDealIntent":
-		response = HandleFrontpageDealIntent(request)
-	case "PopularDealIntent":
-		response = HandlePopularDealIntent(request)
+		//case "FrontpageDealIntent":
+		//	response = HandleFrontpageDealIntent(request)
+		//case "PopularDealIntent":
+		//	response = HandlePopularDealIntent(request)
 	case alexa.HelpIntent:
 		response = HandleHelpIntent(request)
 	case "AboutIntent":
@@ -40,6 +40,16 @@ func IntentDispatcher(request alexa.Request) alexa.Response {
 }
 
 func HandleTodaysFishRatingIntent(request alexa.Request) alexa.Response {
+	defer func() alexa.Response {
+		var resp alexa.Response
+		if r := recover(); r != nil {
+			resp = alexa.NewSimpleResponse("Today's Fishing Forecast", "You caught me! Like a young fish, I'm still learning. "+
+				"Please be patient with me, I'll have forecasts for you soon!")
+		}
+
+		return resp;
+	}()
+
 	deviceId := request.Context.System.Device.DeviceID
 	apiAccessToken := request.Context.System.APIAccessToken
 	apiEndpoint := request.Context.System.APIEndpoint
@@ -52,87 +62,88 @@ func HandleTodaysFishRatingIntent(request alexa.Request) alexa.Response {
 		Client: http.Client{},
 	}
 
-	resp, _ := DeviceLocService.GetDeviceLocation()
+	resp, err := DeviceLocService.GetDeviceLocation()
+	if err != nil {
+		panic("trouble getting your zip code area, please ensure you have granted permission for this so that " +
+			"Da Fish can determine the fishing forecast for your area.")
+	}
 	log.Print(resp)
 	// Get Geocode coordinates from retrieved location
 	GeocodeService = services.GeocodeService{
-		URL: cfg.GeoUrl,
-		UsrIp: defaultUserIp,
+		URL:           cfg.GeoUrl,
+		UsrIp:         defaultUserIp,
 		CountryRegion: resp.CountryCode,
-		PostalCode: resp.PostalCode,
-		Key: cfg.GeoKey,
+		PostalCode:    resp.PostalCode,
+		Key:           cfg.GeoKey,
+		Client:        http.Client{},
 	}
 
-	geoPoint, _ := GeocodeService.GetGeoPoint()
+	geoPoint, err := GeocodeService.GetGeoPoint()
+	if err != nil {
+		panic("trouble getting information on the area ")
+	}
 	log.Printf("Geo: {lat: %f, lon: %f}", geoPoint.Coordinates[0], geoPoint.Coordinates[1])
 
 	//Get Fishing Forecast using coordinates
-	//TODO - Start Here,  use GeoPoint to get  FishRating
-
-	return alexa.NewSimpleResponse("Today's Fishing Forecast", "You caught me! Like a young fish, I'm still learning. "+
-		"Please be patient with me, I'll have forecasts for you soon!")
-}
-
-//TODO Remove
-func HandleFrontpageDealIntent(request alexa.Request) alexa.Response {
-	feedResponse, _ := RequestFeed("frontpage")
-	var builder alexa.SSMLBuilder
-	builder.Say("Here are the current frontpage deals:")
-	builder.Pause("1000")
-	for _, item := range feedResponse.Channel.Item {
-		builder.Say(item.Title)
-		builder.Pause("1000")
+	ForecasterService = services.ForecasterService{
+		URL:    cfg.FishRatingUrl,
+		Lat:    geoPoint.Coordinates[0],
+		Lon:    geoPoint.Coordinates[1],
+		Client: http.Client{},
 	}
-	return alexa.NewSSMLResponse("Frontpage Deals", builder.Build())
-}
 
-//TODO Remove
-func HandlePopularDealIntent(request alexa.Request) alexa.Response {
-	return alexa.NewSimpleResponse("Popular Deals", "Popular deal data here")
+	fr, err := ForecasterService.GetCurrentFishingRating()
+	if err != nil {
+		panic("Trouble Getting Fishing Forecast ")
+	}
+	var (
+		t string
+		r uint
+		w float64
+	)
+	if fr[0].Rating > fr[1].Rating {
+		t, r, w = "right now", fr[0].Rating, fr[0].WindSpeed
+	} else {
+		t, r, w = "two hours from now", fr[1].Rating, fr[1].WindSpeed
+	}
+
+	var fcstBuilder alexa.SSMLBuilder
+	if r < 3 {
+		fcstBuilder.Say("It looks like the best time to go fishing over the next couple of hours is, ")
+		fcstBuilder.Pause("250")
+		fcstBuilder.Say("well, ")
+		fcstBuilder.Pause("250")
+		fcstBuilder.Say(
+			fmt.Sprintf("probably some other time with a top rating well below average and a wind speed of %f.", w))
+	} else if r >= 3 && r <= 4 {
+		fcstBuilder.Say(fmt.Sprintf("It looks like a decent or possibly better time to go fishing %s with a forecast rating "+
+			"just on the plus side.", t))
+		fcstBuilder.Pause("500")
+		fcstBuilder.Say(fmt.Sprintf("The wind speed is listed at %f.", w))
+	} else {
+		fcstBuilder.Say("The fish appear to be biting!")
+		fcstBuilder.Pause("750")
+		fcstBuilder.Say(fmt.Sprintf("Over the next couple hours the fishing looks great "+
+			"with the best time to go being %s with a forecast rating well over the norm. ", t))
+		fcstBuilder.Pause("500")
+		fcstBuilder.Say(fmt.Sprintf("The wind speed is listed at %f.", w))
+	}
+
+	return alexa.NewSSMLResponse("Today's Fishing Forecast", fcstBuilder.Build())
 }
 
 func HandleHelpIntent(request alexa.Request) alexa.Response {
-	// TODO
 	var builder alexa.SSMLBuilder
 	builder.Say("Here are some of the things you can ask:")
 	builder.Pause("1000")
-	builder.Say("Give me the frontpage deals.")
+	builder.Say("Give me todays fishing forecast.")
 	builder.Pause("1000")
-	builder.Say("Give me the popular deals.")
-	return alexa.NewSSMLResponse("Slick Dealer Help", builder.Build())
+	builder.Say("When is the best time to go fishing?")
+	return alexa.NewSSMLResponse("Da Fish Forecaster Help", builder.Build())
 }
 
 func HandleAboutIntent(request alexa.Request) alexa.Response {
 	return alexa.NewSimpleResponse("About", "Da Fish was created by HuntX in Saint Louis, Missouri so that he couldn't talk himself out of going fishing by using the excuse that conditions may not be optimal and figuring it out takes too much time to look up.")
-}
-
-// TODO - Delete this or rework for JSON
-type FeedResponse struct {
-	Channel struct {
-		Item []struct {
-			Title string `xml:"title"`
-			Link  string `xml:"link"`
-		} `xml:"item"`
-	} `xml:"channel"`
-}
-
-func RequestFeed(mode string) (FeedResponse, error) {
-	endpoint, _ := url.Parse("https://slickdeals.net/newsearch.php")
-	queryParams := endpoint.Query()
-	queryParams.Set("mode", mode)
-	queryParams.Set("searcharea", "deals")
-	queryParams.Set("searchin", "first")
-	queryParams.Set("rss", "1")
-	endpoint.RawQuery = queryParams.Encode()
-	response, err := http.Get(endpoint.String())
-	if err != nil {
-		return FeedResponse{}, err
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		var feedResponse FeedResponse
-		xml.Unmarshal(data, &feedResponse)
-		return feedResponse, nil
-	}
 }
 
 func Handler(request alexa.Request) (alexa.Response, error) {
@@ -147,3 +158,49 @@ func init() {
 func main() {
 	lambda.Start(Handler)
 }
+
+////TODO Remove
+//func HandleFrontpageDealIntent(request alexa.Request) alexa.Response {
+//	feedResponse, _ := RequestFeed("frontpage")
+//	var builder alexa.SSMLBuilder
+//	builder.Say("Here are the current frontpage deals:")
+//	builder.Pause("1000")
+//	for _, item := range feedResponse.Channel.Item {
+//		builder.Say(item.Title)
+//		builder.Pause("1000")
+//	}
+//	return alexa.NewSSMLResponse("Frontpage Deals", builder.Build())
+//}
+//
+////TODO Remove
+//func HandlePopularDealIntent(request alexa.Request) alexa.Response {
+//	return alexa.NewSimpleResponse("Popular Deals", "Popular deal data here")
+//}
+//// TODO - Delete this or rework for JSON
+//type FeedResponse struct {
+//	Channel struct {
+//		Item []struct {
+//			Title string `xml:"title"`
+//			Link  string `xml:"link"`
+//		} `xml:"item"`
+//	} `xml:"channel"`
+//}
+//
+//func RequestFeed(mode string) (FeedResponse, error) {
+//	endpoint, _ := url.Parse("https://slickdeals.net/newsearch.php")
+//	queryParams := endpoint.Query()
+//	queryParams.Set("mode", mode)
+//	queryParams.Set("searcharea", "deals")
+//	queryParams.Set("searchin", "first")
+//	queryParams.Set("rss", "1")
+//	endpoint.RawQuery = queryParams.Encode()
+//	response, err := http.Get(endpoint.String())
+//	if err != nil {
+//		return FeedResponse{}, err
+//	} else {
+//		data, _ := ioutil.ReadAll(response.Body)
+//		var feedResponse FeedResponse
+//		xml.Unmarshal(data, &feedResponse)
+//		return feedResponse, nil
+//	}
+//}
--- services/fishing-forecast-service.go
+++ services/fishing-forecast-service.go
@@ -1,1 +1,80 @@
 package services
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type ForecasterService struct {
+	URL string
+	Lat float64
+	Lon float64
+	Client http.Client
+}
+type ForecasterResponse struct {
+	Currently struct {
+		ApparentTemperature float64 `json:"apparentTemperature"`
+		CloudCover float64 `json:"cloudCover"`
+		DewPoint float64 `json:"dewPoint"`
+		Humidity float64 `json:"humidity"`
+		Icon string `json:"icon"`
+		NearestStormBearing int `json:"nearestStormBearing"`
+		NearestStormDistance int `json:"nearestStormDistance"`
+		Ozone float64 `json:"ozone"`
+		PrecipIntensity float64 `json:"precipIntensity"`
+		PrecipProbability float64 `json:"precipProbability"`
+		//pressure: 1009.26
+		Summary string `json:"summary"`
+		Temperature float64 `json:"temperature"`
+		Time int64
+		//uvIndex: 0
+		//visibility: 10
+		WindBearing int `json:"windBearing"`
+		WindGust float64 `json:"windGust"`
+		WindSpeed float64 `json:"windSpeed"`
+	} `json:"currently"`
+	Hourly[] Hour `json:"hourly"`
+}
+
+type Hour struct {
+	Icon string `json:"icon"`
+	PrecipProbability float64 `json:"precipProbability"`
+	Rating uint `json:"rating"`
+	Temperature float64 `json:"temperature"`
+	Time int64 `json:"time"`
+	WindSpeed float64 `json:"windSpeed"`
+}
+
+func (s *ForecasterService) GetCurrentFishingRating() ([]Hour, error) {
+	resp, err := s.GetFullFishingForecast()
+	if err != nil {
+		log.Printf("Failed to get Fishing Forecast: %s", err.Error())
+		return []Hour{}, err
+	}
+	//For now Let's just return now and 2 hours from now.
+	response := []Hour{resp.Hourly[0], resp.Hourly[2]}
+	return response, nil
+}
+
+func (s *ForecasterService) GetFullFishingForecast() (*ForecasterResponse, error) {
+	reqUrl := fmt.Sprintf("%s?lat=%flon=%f", s.URL, s.Lat, s.Lon)
+	req, err := http.NewRequest(http.MethodGet, reqUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := s.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	forecasterResponse := ForecasterResponse{}
+
+	if err := json.NewDecoder(resp.Body).Decode(&forecasterResponse); err != nil {
+		return nil, err
+	}
+	return &forecasterResponse, nil
+}
